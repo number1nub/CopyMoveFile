3a310012a0e75420662a753d411fd21e879912b2
Rameen B <number1nub@gmail.com>
2014-10-29 01:41:18 -0500

    Version 1.0.0 - Functioning copy & move commands with overwrite protection - TODO: Create help output - TODO: Allow multiple file copy/move - TODO: Allow copy/move for folders

--- CopyMoveFile/Program.cs
+++ CopyMoveFile/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Arguments;
+
 
 namespace CopyMoveFile
 {
@@ -10,6 +9,128 @@ namespace CopyMoveFile
 	{
 		static void Main(string[] args)
 		{
+			var oArgs  = new Args(args);
+
+			string action = oArgs["action"] == null ? args[1] : oArgs["action"];
+			string inPath = oArgs["inPath"] != null ? oArgs["inPath"] : (oArgs["inFile"] != null ? oArgs["inFile"] : "");
+			string outPath = oArgs["outPath"] != null ? oArgs["outPath"] : (oArgs["outFile"] != null ? oArgs["outFile"] : "");
+
+			if (string.IsNullOrEmpty(inPath) || string.IsNullOrEmpty(outPath))
+			{
+				Console.WriteLine("\nInvalid or missing file path(s).\n\nUse -? to view available options");
+				Console.ReadLine();
+				Environment.Exit(1);
+			}
+			if (string.IsNullOrEmpty(action))
+			{
+				Console.WriteLine("\nInvalid action parameter.\n\nUse -? to view available options");
+				Console.ReadLine();
+				Environment.Exit(1);
+			}
+
+			// Verify that source file exists
+			var inFile = new FileInfo(inPath);
+			var outFile = new FileInfo(outPath);
+			var inAttrs = inFile.Attributes;
+			var outAttrs = outFile.Attributes;
+
+			if (!inFile.Exists)
+			{
+				Console.WriteLine("\nInvalid input file. Unable to locate \"{0}\"", inPath);
+				Console.ReadLine();
+				Environment.Exit(1);
+			}
+			
+			// Determine whether the paths are files or folders
+			bool outIsDir = (outAttrs & FileAttributes.Directory) != 0 ? true : false;
+			bool inIsDir = (inAttrs & FileAttributes.Directory) != 0 ? true : false;
+
+			if (inIsDir)
+			{
+				Console.WriteLine("\nInvalid input file path. Currently only functional for single files.");
+				Console.ReadLine();
+				Environment.Exit(1);				
+			}
+
+
+
+			switch (action)
+			{
+				case "copy":					
+					if (!outIsDir)
+					{	//
+						// Copy file to a given file name
+						//
+						if (outFile.Exists)
+						{
+							Console.WriteLine("The specified output file already exists. Overwrite? [y/n]: ");
+							string overWrite = Console.ReadLine().ToUpper();
+							if (overWrite != "Y" && overWrite != "YES")
+								Environment.Exit(1);
+						}
+					}
+					else
+					{	//
+						// Copy file to a given directory
+						//
+						outPath = Path.Combine(outPath, inFile.Name);
+						if (new FileInfo(outPath).Exists)
+						{ 
+							Console.WriteLine("The specified output file already exists. Overwrite? [y/n]: ");
+							string overWrite = Console.ReadLine().ToUpper();
+							if (overWrite != "Y" && overWrite != "YES")
+								Environment.Exit(1);
+						}
+					}
+					inFile.CopyTo(outPath, true);
+					break;
+
+				case "move":
+					if (!outIsDir)
+					{	//
+						// Move file to a given file name
+						//
+						if (outFile.Exists)
+						{
+							Console.WriteLine("The specified output file already exists. Overwrite? [y/n]: ");
+							string overWrite = Console.ReadLine().ToUpper();
+							if (overWrite == "Y" || overWrite == "YES")
+							{
+								new FileInfo(outFile.FullName + ".bak").Delete();
+								outFile.MoveTo(outFile.FullName + ".bak");
+							}
+							else
+								Environment.Exit(1);
+						}
+					}
+					else
+					{	//
+						// Move file to a given directory
+						//
+						outPath = Path.Combine(outPath, inFile.Name);
+						outFile = new FileInfo(outPath);
+						if (outFile.Exists)
+						{
+							Console.WriteLine("The specified output file already exists. Overwrite? [y/n]: ");
+							string overWrite = Console.ReadLine().ToUpper();
+							if (overWrite == "Y" || overWrite == "YES")
+							{
+								new FileInfo(outFile.FullName + ".bak").Delete();
+								outFile.MoveTo(outFile.FullName + ".bak");
+							}
+							else
+								Environment.Exit(1);
+						}
+					}
+					inFile.MoveTo(outPath);
+					break;
+
+				default:					
+					Console.WriteLine("Invalid action parameter.\n\nUse -? to view available options");
+					Console.ReadLine();
+					Environment.Exit(1);
+					break;
+			}
 		}
 	}
 }
--- CopyMoveFile/Properties/AssemblyInfo.cs
+++ CopyMoveFile/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CopyMoveFile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CopyMoveFile CmdLine Utility")]
+[assembly: AssemblyDescription("Command line utility that allows simple copying and moving of files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CopyMoveFile")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("WSNHapps")]
+[assembly: AssemblyProduct("CopyMoveFile CmdLine Utility")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
